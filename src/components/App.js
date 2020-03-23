26c655ab350105958a641eecbe3a556d7c1af7e5
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-03-23 21:24:28 +0100

    Adds form and renders tasks

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import nanoid from "nanoid";
 
-const App = () => {
-  return <div>App</div>;
-};
+function handleOnSubmit(e, todos, setTodos, input, setInput) {
+  e.preventDefault();
+  const id = nanoid();
+  setTodos([...todos, { id, text: input, completed: false }]);
+  setInput("");
+}
+
+function handleOnChange(id, setTodos, todos) {
+  setTodos(
+    todos.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    )
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+  return (
+    <div>
+      <form
+        onSubmit={event =>
+          handleOnSubmit(event, todos, setTodos, input, setInput)
+        }
+      >
+        <input onChange={event => setInput(event.target.value)} value={input} />
+      </form>
+      {todos.map(todo => (
+        <div>
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => handleOnChange(todo.id, setTodos, todos)}
+          />
+          {todo.text}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default App;
