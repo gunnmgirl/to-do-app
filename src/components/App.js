9e2a985ec022998ef4acb97765d56121f3230d8e
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-03-24 21:14:26 +0100

    Adds style to list of tasks

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,28 @@
 import React, { useState } from "react";
 import nanoid from "nanoid";
+import styled from "styled-components";
+import CheckCircle from "./CheckCircle";
+import Circle from "./Circle";
+
+const Task = styled.div`
+  border-bottom: 0.5px solid #e6e6e6;
+  padding-bottom: 10px;
+  padding-top: 10px;
+  display: flex;
+  align-items: center;
+`;
+
+const Text = styled.span`
+  font-family: "Segoe";
+  color: #0d0d0d;
+  font-size: 1.1rem;
+`;
+
+const StyledButton = styled.button`
+  border: none;
+  padding-right: 14px;
+  background-color: #ffffff;
+`;
 
 function handleOnSubmit(e, todos, setTodos, input, setInput) {
   e.preventDefault();
@@ -8,7 +31,7 @@ function handleOnSubmit(e, todos, setTodos, input, setInput) {
   setInput("");
 }
 
-function handleOnChange(id, setTodos, todos) {
+function handleOnClick(id, setTodos, todos) {
   setTodos(
     todos.map(todo =>
       todo.id === id ? { ...todo, completed: !todo.completed } : todo
@@ -26,17 +49,29 @@ function App() {
           handleOnSubmit(event, todos, setTodos, input, setInput)
         }
       >
-        <input onChange={event => setInput(event.target.value)} value={input} />
+        <input
+          placeholder="Add a task"
+          onChange={event => setInput(event.target.value)}
+          value={input}
+        />
       </form>
       {todos.map(todo => (
-        <div>
-          <input
-            type="checkbox"
-            checked={todo.completed}
-            onChange={() => handleOnChange(todo.id, setTodos, todos)}
-          />
-          {todo.text}
-        </div>
+        <Task>
+          <StyledButton>
+            {todo.completed ? (
+              <CheckCircle
+                color="#3385ff"
+                onClick={() => handleOnClick(todo.id, setTodos, todos)}
+              />
+            ) : (
+              <Circle
+                color="#3385ff"
+                onClick={() => handleOnClick(todo.id, setTodos, todos)}
+              />
+            )}
+          </StyledButton>
+          <Text>{todo.text}</Text>
+        </Task>
       ))}
     </div>
   );
