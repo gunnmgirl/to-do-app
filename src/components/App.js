c4e37cbc5c6b77d98e16993c6e6f98376c5468d0
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-03-26 19:14:44 +0100

    Adds lists

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
-import nanoid from "nanoid";
 import styled from "styled-components";
+import nanoid from "nanoid";
+
 import CheckCircle from "./CheckCircle";
 import Circle from "./Circle";
 import Plus from "./Plus";
+import Form from "./Form";
+import List from "./List";
 
 const IconTextWrapper = styled.div`
   border-bottom: 0.5px solid #e6e6e6;
@@ -31,63 +34,96 @@ const StyledButton = styled.button`
   background-color: #ffffff;
 `;
 
-function handleOnSubmit(e, todos, setTodos, input, setInput) {
+const Sidebar = styled.div`
+  width: 20%;
+  height: 100%;
+`;
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const Tasks = styled.div`
+  width: 70%;
+`;
+
+function handleOnSubmit(e, input, setInput, lists, activeList) {
   e.preventDefault();
-  const id = nanoid();
-  input
-    ? setTodos([...todos, { id, text: input, completed: false }])
-    : setTodos([...todos]);
+  lists.map(list =>
+    list.id === activeList
+      ? list.tasks.push({ text: input, id: nanoid(), completed: false })
+      : list.tasks
+  );
   setInput("");
 }
 
-function handleOnClick(id, setTodos, todos) {
-  setTodos(
-    todos.map(todo =>
-      todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    )
-  );
-}
-
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [activeList, setActiveList] = useState(null);
   const [input, setInput] = useState("");
+  const [lists, setLists] = useState([
+    {
+      id: nanoid(),
+      name: "prva",
+      tasks: [
+        { text: "1", id: nanoid(), completed: false },
+        { text: "2", id: nanoid(), completed: false },
+        { text: "3", id: nanoid(), completed: false }
+      ]
+    },
+    {
+      id: nanoid(),
+      name: "druga",
+      tasks: [
+        { text: "4", id: nanoid(), completed: false },
+        { text: "5", id: nanoid(), completed: false },
+        { text: "6", id: nanoid(), completed: false }
+      ]
+    }
+  ]);
+  const [task, setTask] = useState();
   return (
-    <div>
-      <IconTextWrapper>
-        <StyledButton>
-          <Plus color="#bfbfbf" />
-        </StyledButton>
+    <MainContainer>
+      <Sidebar>
+        {lists.map(list => (
+          <IconTextWrapper>
+            <StyledButton>
+              <List color="#3385ff" />
+            </StyledButton>
+            <Text onClick={() => setActiveList(list.id)}>{list.name}</Text>
+          </IconTextWrapper>
+        ))}
+        <Form lists={lists} setLists={setLists} placeholder="New list" />
+      </Sidebar>
+      <Tasks>
+        {lists.map(list =>
+          list.id === activeList
+            ? list.tasks.map(task => (
+                <IconTextWrapper>
+                  <StyledButton>
+                    <Circle />
+                  </StyledButton>
+                  <Text>{task.text}</Text>
+                </IconTextWrapper>
+              ))
+            : null
+        )}
+
         <form
-          onSubmit={event =>
-            handleOnSubmit(event, todos, setTodos, input, setInput)
-          }
+          onSubmit={e => handleOnSubmit(e, input, setInput, lists, activeList)}
         >
-          <InputBox
-            placeholder="Add a task"
-            onChange={event => setInput(event.target.value)}
-            value={input}
-          />
+          <IconTextWrapper>
+            <StyledButton>
+              <Circle />
+            </StyledButton>
+            <InputBox
+              placeholder="Add a task"
+              value={input}
+              onChange={e => setInput(e.target.value)}
+            />
+          </IconTextWrapper>
         </form>
-      </IconTextWrapper>
-      {todos.map(todo => (
-        <IconTextWrapper>
-          <StyledButton>
-            {todo.completed ? (
-              <CheckCircle
-                color="#3385ff"
-                onClick={() => handleOnClick(todo.id, setTodos, todos)}
-              />
-            ) : (
-              <Circle
-                color="#3385ff"
-                onClick={() => handleOnClick(todo.id, setTodos, todos)}
-              />
-            )}
-          </StyledButton>
-          <Text completed={todo.completed}>{todo.text}</Text>
-        </IconTextWrapper>
-      ))}
-    </div>
+      </Tasks>
+    </MainContainer>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import nanoid from "nanoid";
+import Plus from "./Plus";
+
+const InputBox = styled.input`
+  border: 0;
+`;
+
+const IconTextWrapper = styled.div`
+  border-bottom: 0.5px solid #e6e6e6;
+  padding-bottom: 10px;
+  padding-top: 10px;
+  display: flex;
+  align-items: center;
+`;
+
+const StyledButton = styled.button`
+  border: 0;
+  padding-right: 14px;
+  background-color: #ffffff;
+`;
+
+function handleOnSubmit(e, lists, setLists, input, setInput) {
+  e.preventDefault();
+  const id = nanoid();
+  input
+    ? setLists([...lists, { id, name: input, tasks: [{}] }])
+    : setLists([...lists]);
+  setInput("");
+}
+
+function Form({ lists, setLists, placeholder }) {
+  const [input, setInput] = useState("");
+  return (
+    <form
+      onSubmit={event =>
+        handleOnSubmit(event, lists, setLists, input, setInput)
+      }
+    >
+      <IconTextWrapper>
+        <StyledButton>
+          <Plus color="#3385ff" />
+        </StyledButton>
+        <InputBox
+          placeholder={placeholder}
+          onChange={event => setInput(event.target.value)}
+          value={input}
+        />
+      </IconTextWrapper>
+    </form>
+  );
+}
+
+export default Form;
